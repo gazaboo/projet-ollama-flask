c206b1c1434e60eadad4c853904a39ecf3a0df61
gazaboo <florian.dadouchi@gmail.com>
2024-07-18 10:15:25 +0200

    added kits apprenants

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def hello_world():
 
     return render_template('index.html', stories=image_path_and_stories)
  
-
-app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
