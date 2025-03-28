2eb6f9cd2c97d9c05011f605bfcf4c8b291a4570
gazaboo <florian.dadouchi@gmail.com>
2025-03-28 17:36:00 +0100

    updated to gemma3

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template
 import glob
-from utils import create_story
+import ollama
+import os 
 
-# __name__ : double underscore variables (dunders)
 app = Flask(__name__) 
 
 @app.route("/")
 def hello_world():
     urls = glob.glob('static/images/*.jpg')
+    abs_urls = [os.path.abspath(url) for url in urls]
     
     image_path_and_stories = []
-    for url in urls:
+    for url in abs_urls:
         story = create_story( url )
-        url_html = '../' + url
+        url_html = 'images/' + url.split('/')[-1]
         image_path_and_stories.append( (story, url_html) )
 
     return render_template('index.html', stories=image_path_and_stories)
- 
+
+def create_story(url):
+    query = """
+    Peux-tu écrire une histoire pour enfants inspirée de l'image. 
+    Ecris l'histoire en français. 
+    """
+    response = ollama.chat(model='gemma3:4b', messages=[{
+        'role': 'user', 
+        'content': query,
+        'images': [url]
+    }])
+    return response['message']['content']
+
+     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
